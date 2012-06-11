d12d7bb159765a29ef4b54e721eaef735ab190bd
alfredkam <akam@adcnx.com>
2012-06-11 17:21:11 -0400

    updated ux

---
+++ plugin.module.js
@@ -0,0 +1,19 @@
+(function() {
+	$.plugin = {
+		onLoad : function() {
+			
+		},
+		onTouchStart : function() {
+
+		},
+		onTouchMove : function() {
+
+		},
+		onTouchEnd : function() {
+
+		},
+		onEachStage : function() {
+
+		},
+	};
+})();
