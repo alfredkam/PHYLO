973b983ce87d8a2b37280a92d86f35058763b54a
Jerome Waldispuhl <waldispuhl@gmail.com>
2012-08-28 23:05:48 -0400

    fixed new bug (table were not properly display at 2nd visit)

--- js/page/main.page.js
+++ js/page/main.page.js
@@ -3,8 +3,15 @@
 		ranking : function() {
             // FIXME? protocal use POST not GET
             //this.protocal("http://phylo.cs.mcgill.ca/phpdb/fullrankingsget.php?lang=" + window.langOpt.toUpperCase());
-            this.protocal("content/ranking.html");
-            $.getScript("js/ranking.js");            
+            $.ajax({
+				//url : "http://phylo.cs.mcgill.ca/phpdb/fullrankingsget.php?lang=" + window.langOpt.toUpperCase(),
+                url : "content/ranking.html",
+				type : "post",
+			}).done(function(re) {
+                $("#mid-panel").html(re);
+                $.getScript("js/ranking.js");
+            });
+            
 		},
 		history : function() {
 			this.protocal("content/history.html");
