b9578bd3f3cc1fc8fdab0360fa75de3db43fc235
alfredkam <akam@adcnx.com>
2012-09-23 21:08:33 -0400

    updated endgame screen

--- js/game/endGame.theme.js
+++ js/game/endGame.theme.js
@@ -7,7 +7,8 @@
 			$.protocal.sendEndGameScore("completed", function(data) {
 				self.events();
 				self.score("completed",data.best_score);
-				var msg = "<b>Congratulations!</b> You have solved the puzzle";
+				//var msg = "<b>Congratulations!</b> You have solved the puzzle";
+				var msg = window.lang.body.play.gameselect["end of game"]["field 3"];
 				$("#endGame-text").html(msg);
 				$("#endGame-learnMore-content").html("This disease is related to diseases etc, you are helping...etc");
 				$("#endGame").fadeIn();
@@ -20,7 +21,8 @@
 			$.protocal.sendEndGameScore("bail", function(data) {
 				self.events();
 				self.score("bail",data.best_score);
-				var msg = "Too bad! You did not succeed to solve this puzzle!";
+				//var msg = "Too bad! You did not succeed to solve this puzzle!";
+				var msg = window.lang.body.play.gameselect["end of game"]["field 4"];
 				$("#endGame-text").html(msg);
 				$("#endGame-learnMore-content").html("This disease is related to diseases etc, you are helping...etc");
 				$("#endGame").fadeIn();
@@ -52,6 +54,18 @@
 		//events for the end game messages
 		//new game or replay game
 		events : function() {
+			
+			$("#endGame-learnMore-content").hide();
+		
+			$("#endGame-learnMore-tag button").unbind().click(function() {
+				$("#endGame-learnMore-content").slideToggle("fast",function() {
+
+				});
+			});
+			
+						
+
+
 			$("#endGame-new button").unbind().click(function() {
 				/*
 				$.main.clear();
