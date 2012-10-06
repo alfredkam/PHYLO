c388bdb39850a5bffbb4466dea30288b77e606e9
alfredkam <akam@adcnx.com>
2012-10-06 01:20:35 -0400

    updated bugs

---
+++ js/RNA/sequence.core.js
@@ -0,0 +1,10 @@
+(function() {
+	if($.rna == undefined)
+		$.rna = {};
+	if($.rna.sequence == undefined)
+		$.rna.sequence = {};
+	var rnaSeq = $.rna.sequence;
+	rnaSeq.build = function() {
+		
+	};
+})();
