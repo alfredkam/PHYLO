3860594155e335e2cc1f88fc4f63b35c227c794c
alfredkam <akam@adcnx.com>
2012-10-16 11:24:53 -0400

    added theme box

--- js/theme/main.theme.js
+++ js/theme/main.theme.js
@@ -1,3 +1,17 @@
 (function() {
 
+	$("#customize").click(function(event) {
+		event.stopPropagation();
+		
+		$(".customize").show();
+		$(".customize-bg").css({
+			width: document.width,
+			height: document.height
+		});
+	});
+
+	$(".customize-cancel").click(function() {
+		$(".customize").hide();
+	});
+
 })();
