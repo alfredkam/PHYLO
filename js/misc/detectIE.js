120b8c336b4c89d5c443a9ae0cf4c36263286965
alfredkam <akam@adcnx.com>
2012-10-22 22:38:50 -0400

    updated detect ie script

--- js/misc/detectIE.js
+++ js/misc/detectIE.js
@@ -1,7 +1,19 @@
 (function() {
-	var str =  navigator.userAgent.toLowerCase().search("MSIE 8.0")
+	var str =  navigator.userAgent.toString().toLowerCase();
 	
 	if(str.search(/MSIE (8.0|7.0|6.0|5.0)/) > -1) {
-		console.log("IE");
+	//	console.log("IE");
+		$(".warning-cancel").hide();
+		$(".warning-ok").hide();
+	
+		$(".warning-msg").html("Oops! Currently we do not support IE 8 and below.<br>  Please either upgrade your browser or use an alternate.<br>If not please click here for flash");
+		
+
+		$(".warning-bg").css({
+			height: $(document).height(),
+			width: $(document).width(),
+		});
+		$(".warning-bg").fadeIn();
+		$(".warning").fadeIn();
 	}
 })();
