f22b9d30103c64fad8d895277b00d160fd3d80fd
alfredkam <kam.alfred@gmail.com>
2012-11-16 13:34:48 -0500

    added flash url

--- js/misc/detectIE.js
+++ js/misc/detectIE.js
@@ -5,7 +5,7 @@
 		$(".warning-cancel").hide();
 		$(".warning-ok").hide();
 	
-		$(".warning-msg").html("<i class='icon-remove'></i><br><b>Aww Snap!</b><br> We currently do not support IE 8 and below.<br>  Please either upgrade your browser to IE9 or above,  or use an alternate browser like chrome or firefox.<br>If not please click here for flash");
+		$(".warning-msg").html("<i class='icon-remove'></i><br><b>Aww Snap!</b><br> We currently do not support IE 8 and below.<br>  Please either upgrade your browser to IE9 or above,  or use an alternate browser like chrome or firefox.<br>If not please <a href='http://phylo.cs.mcgill.ca/archive/flash/eng/'>click here</a> for flash");
 
 		$(".warning-bg").css({
 			height: $(document).height(),
