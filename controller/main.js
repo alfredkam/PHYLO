72381047a78736a969895f1ab1a67c4eef7794ce
alfredkam <kam.alfred@gmail.com>
2012-11-19 10:48:00 -0500

    integration init

--- controller/main.js
+++ controller/main.js
@@ -1,9 +1,43 @@
 (function(){
 	requirejs.config({
-		baseURl : 'js/blueprint',
+		baseUrl : 'js/blueprint',
 		paths : {
 		     DNA : '../DNA',
 		     RNA : '../RNA',
+		     theme : '../theme',
 		},
+		shim : {
+			'backbone' : {
+				deps : ['underscore', 'jquery'],
+			},
+			'yepnope' : {
+				exports : 'yepnope',
+			},
+			'bootstrap' : {
+				deps : ['jquery','jquery-ui'],
+			},
+			'mustache' : {
+				deps : ['jquery'],
+			}
+			'DNA/
+		}
 	});
+	//loads tablet UX
+	require(['yepnope'],
+		function(yepnope) {
+			window.isTablet = navigator.userAgent.match(/(iPad|Android .* Chrome\/[.0-9]* (?!Mobile)|Opera Tablet|Android .* (?!Mobile)|Tablet)/i) != null;
+			yepnope({
+				test : isTablet,
+				yep : 'css/tablet.css',
+				nope : ['css/media1280.css','css/media1180.css','css/media1024.css'],
+			});
+			//test mode script injection
+			yepnope({
+				test : window.DEV.enableTabletMode,
+				yep : 'css/tablet.css',
+			});
+		}
+	);
+	require(['bootstrap','mustache']);
+	require(['backbone']);
 })();
