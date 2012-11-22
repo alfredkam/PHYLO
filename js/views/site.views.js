f18913a24cf6e0d604f1517515bc11f710ff2c33
alfredkam <kam.alfred@gmail.com>
2012-11-21 19:41:20 -0500

    updated framwork

--- js/views/site.views.js
+++ js/views/site.views.js
@@ -15,7 +15,7 @@
 			//edit language href to selected tab
 			$(".language-list a").each(function() {
 				var href = $(this).attr("href").replace(/#!.*$/,"");
-				$(this).attr("href",href+"#!"+tag);
+				$(this).attr("href",href+"#!/"+$(this).attr("name")+"/"+tag);
 			});
 		};
 	
@@ -23,42 +23,66 @@
 		var playView = Backbone.View.extend({
 			render : function(lang) {
 				selectTab("play");
-				request.get("templates/play.html",lang);
+				request.getTemplate("templates/play.html",lang);
 			},
 		});
 
 		var historyView = Backbone.View.extend({
 			render : function(lang) {
 				selectTab("history");
-
+				$("#mid-panel").html("<div id='history-wrapper'></div>");
+				request.post(
+					"http://phylo.cs.mcgill.ca/phpdb/userrecordget.php?username=" + window.guest,
+					function(re) {
+						if($("#history-wrapper").length != 0) {
+							$("#history-wrapper").html(re);
+							require(['history/DT_bootstrap_history'],function() {
+								historyTable.init();
+							});
+						}
+						request.complete();
+					}
+				);
 			},
 		});
 		
 		var aboutView = Backbone.View.extend({
 			render : function(lang) {
 				selectTab("about");
-				request.get("templates/about.html",lang);
+				request.getTemplate("templates/about.html",lang);
 			},
 		});
 
 		var rankingView = Backbone.View.extend({
 			render : function(lang) {
 				selectTab("ranking");
-
+				$("#mid-panel").html("<div id='ranking-wrapper'></div>");
+				request.post(	
+					"http://phylo.cs.mcgill.ca/phpdb/fullrankingsget.php?lang=" + window.langOpt.toUpperCase(),
+					function(re) {
+						if($("#ranking-wrapper").length != 0) {
+							$("#ranking-wrapper").html(re);
+							require(['ranking/DT_bootstrap_ranking'],function() {
+								rankingTable.init();
+							});
+						}
+						request.complete();
+					}
+				);
 			},
 		});
 		
 		var creditsView = Backbone.View.extend({
 			render : function(lang) {
 				selectTab("credits");
-				request.get("templates/credits.html",lang);
+				request.getTemplate("templates/credits.html",lang);
 			},
 		});
 
 		var tutorialView = Backbone.View.extend({
 			render : function(lang) {
 				selectTab("tutorial");
-				request.get("templates/tutorial.html",lang);
+				request.getTemplate("templates/tutorial.html",lang);
 			},
 		});
 
