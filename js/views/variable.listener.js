a28b8254ad1f1b8a161a6cef8135bfa7a486a40d
alfredkam <kam.alfred@gmail.com>
2012-11-23 11:28:24 -0500

    test

--- js/views/variable.listener.js
+++ js/views/variable.listener.js
@@ -6,6 +6,7 @@
 	],function($, _, Backbone) {
 		var obj = Backbone.View.extend({
 			change : function(_default, _diffFactor, _stepFunction, _maxWait, callBack) {
+				console.log(_stepFunction + " < > "+_maxWait);
 				var check = function(_step) {
 					console.log(_step);
 					if(_default != _diffFactor)
@@ -14,7 +15,7 @@
 						return callBack(false);
 					else
 						window.setTimeout(function(_step) {
-							check(_step+_stepFunction);
+							check(parseInt(_step+_stepFunction));
 						},_stepFunction);
 				};
 				check(0);
