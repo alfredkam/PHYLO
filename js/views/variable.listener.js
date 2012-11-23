eb50da6d2d6c8210a0333465816b78c62bfdcaac
alfredkam <kam.alfred@gmail.com>
2012-11-23 11:34:06 -0500

    listener test

--- js/views/variable.listener.js
+++ js/views/variable.listener.js
@@ -9,6 +9,8 @@
 				var check = function(_step) {
 					console.log(_step);
 					console.log(window.guest + " < >" +_default);
+					console.log((_default != _diffFactor? true : false));
+					console.log((_default != window.guest? true : false));
 					if(_default != _diffFactor)
 						return callBack(true);
 					else if(_step >= _maxWait)
