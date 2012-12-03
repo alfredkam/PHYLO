2b46d3ecf871afd9b3b3c2980ff61729c5536621
Jerome Waldispuhl <waldispuhl@gmail.com>
2012-12-03 00:05:40 -0500

    3rd try

--- js/views/site.views.js
+++ js/views/site.views.js
@@ -144,14 +144,14 @@
 						}
 						request.complete();
 					},
-					"js/dummy/ranking.dummy",
-                    request.getTemplate("templates/ranking_legend.html",function(context) {
-                        request.getJsonLang(lang, function(json) {
-                            $("#mid-panel").html(Mustache.render(context,json.body.play));
-                            request.complete();
-                        });
+					"js/dummy/ranking.dummy"
+				);
+                request.getTemplate("templates/ranking_legend.html",function(context) {
+                    request.getJsonLang(lang, function(json) {
+                        $("#mid-panel").append(Mustache.render(context,json.body.play));
+                        request.complete();
                     });
-                );
+                });
 			},
 		});
 		
