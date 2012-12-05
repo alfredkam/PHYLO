d36e06a10adf10bd29e89edcd7df2c79dc12c483
alfredkam <kam.alfred@gmail.com>
2012-12-04 20:26:11 -0500

    test

--- js/DNA/stage.core.js
+++ js/DNA/stage.core.js
@@ -8,7 +8,6 @@
 		round : function() {	
 			//for testing
 			if(this.current < this.last){
-				console.log(this.current);
 				this.current+=1;
 				this.set(this.current);
 			} else if(this.current === this.last) {
