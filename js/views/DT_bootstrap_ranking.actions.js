9655cdc433c9eccb225d3e87c6e4bc14df4bb1fe
Jerome Waldispuhl <waldispuhl@gmail.com>
2012-12-06 16:49:34 -0500

    move elements in datable init

--- js/views/DT_bootstrap_ranking.actions.js
+++ js/views/DT_bootstrap_ranking.actions.js
@@ -107,7 +107,6 @@ function g(){};
     /* Table initialisation */
     $('.table').dataTable( {
         "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-        "sPaginationType": "bootstrap",
         "oLanguage": {
             "sUrl": "js/models/dataTable/datatable_" + window.langOpt.toUpperCase() + ".txt"
         },
@@ -127,6 +126,7 @@ function g(){};
             }
         },
         "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0 ] } ],
+        "sPaginationType": "bootstrap",
     });
                   
                   
