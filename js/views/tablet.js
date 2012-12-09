a318e938f6865a92ccc3379936e5ba44285c5c71
alfredkam <kam.alfred@gmail.com>
2012-12-09 17:09:00 -0500

    test/break

--- js/views/tablet.js
+++ js/views/tablet.js
@@ -1,26 +1,27 @@
 (function() {
 	//loads this script if its tablet
-	define(['jquery'],
-		function($) {
-	$("#tablet-login-template").css({
-		width : $(document).width()
-	});
-
-	var content = $("#login-box").html();
-	$("#login-box").html("");
-	$("#tablet-login-box").html(content+"<br><br><div><a class='m_cancel btn btn-danger'>Cancel</a></div>");
-
-	$("#tablet-login-tag").click(function() {
-		$("#tablet-login-box-bg").css({
-			height: $(document).height(),
+	require( ['jquery'],
+	function($) {
+		
+		$("#tablet-login-template").css({
 			width : $(document).width()
-		}).show();
-		$("#tablet-login-box").show();
-	});
+		});
 
-	$("#tablet-login-box .m_cancel").click(function() {
-		$("#tablet-login-box-bg").hide();
-		$("#tablet-login-box").hide();
-	});
+		var content = $("#login-box").html();
+		$("#login-box").html("");
+		$("#tablet-login-box").html(content+"<br><br><div><a class='m_cancel btn btn-danger'>Cancel</a></div>");
+
+		$("#tablet-login-tag").click(function() {
+			$("#tablet-login-box-bg").css({
+				height: $(document).height(),
+				width : $(document).width()
+			}).show();
+			$("#tablet-login-box").show();
+		});
+
+		$("#tablet-login-box .m_cancel").click(function() {
+			$("#tablet-login-box-bg").hide();
+			$("#tablet-login-box").hide();
+		});
 	});
 })();
