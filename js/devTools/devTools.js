26ed0da8c430c020f805df6b5320b78a30b40323
alfredkam <kam.alfred@gmail.com>
2012-12-11 20:00:25 -0500

    test/break

--- js/devTools/devTools.js
+++ js/devTools/devTools.js
@@ -44,22 +44,22 @@
 		notify : function( json ) {
 			var self = this;
 			try {
-			var isJson;
-			try {
-				isJson = eval(json);
-			} catch(err) {
-				isJson = null;
-			} 
-			if(isJson == null) {
+				var isJson;
+				try {
+					isJson = eval(json);
+				} catch(err) {
+					isJson = null;
+				} 
+				if(isJson == null) {
 					$("#notification").notify("create", { title : "" , text : json });
-			} else {
-				if(json.type == undefined)
-					$("#notification").notify("create", { title : json.title , text : json.text });
-				else if (json.type == "error") 
-					$("#notification").notify("create", "error-template",{ title : json.title , text : json.text },{custom:true});
-				else 
-					$("#notification").notify("create", { title : json.title , text : json.text });
-			}
+				} else {
+					if(json.type == undefined)
+						$("#notification").notify("create", { title : json.title , text : json.text });
+					else if (json.type == "error") 
+						$("#notification").notify("create", "error-template",{ title : json.title , text : json.text },{custom:true});
+					else 
+						$("#notification").notify("create", { title : json.title , text : json.text });
+				}
 			} catch(err) {
 				$("#notification").notify();
 				self.notify(json);
@@ -137,7 +137,7 @@
 			var g = window[name] = new obj;
 			for(var i=0;i<attr.length;i++) {
 				try {
-				g[attr[i][0]] = attr[i][1];
+					g[attr[i][0]] = attr[i][1];
 				} catch(err) {
 				}
 			}
@@ -146,6 +146,7 @@
 	exportSingleton("devTools",g,attr);
 	//export this to notify
 	console.notify = devTools.prompts.notify;
+	/*
 	//initalize notify
 	$(document).ready(function() {
 		try {
@@ -153,5 +154,5 @@
 		} catch(err) {
 			console.log("Error > Notify Module initalization fail");
 		};
-	});
+	});*/
 })();
