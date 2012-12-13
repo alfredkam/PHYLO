ced9782c0380fae9180df5f1e8aacbfc7b0486c8
Jerome Waldispuhl <waldispuhl@gmail.com>
2012-12-13 15:34:55 -0500

    back

--- js/views/site.views.js
+++ js/views/site.views.js
@@ -133,7 +133,6 @@
 			render : function(lang) {
 				selectTab("ranking");
 				$("#mid-panel").html("<div id='ranking-wrapper'></div>\n<div id='ranking-legend'></div>");
-                /*
                 request.post(
 					"http://phylo.cs.mcgill.ca/phpdb/fullrankingsget.php?lang=" + lang.toUpperCase(),
 					function(re) {
@@ -146,7 +145,7 @@
 						request.complete();
 					},
 					"js/dummy/ranking.dummy"
-                );*/
+                );
                 request.getTemplate("templates/ranking_legend.html",function(context) {
                     request.getJsonLang(lang, function(json) {
                         $("#ranking-legend").html(Mustache.render(context,json.body.play));
