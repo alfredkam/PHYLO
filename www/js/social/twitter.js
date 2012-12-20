80ad57f1d02cdc011a10e440c9da1c618b234a04
alfredkam <kam.alfred@gmail.com>
2012-12-20 11:22:56 -0500

    emu

--- www/js/social/twitter.js
+++ www/js/social/twitter.js
@@ -1,17 +1,23 @@
 (function() {
 	var twit = {
 		isAvailable : function() {
-			console.notify("Waiting for twitter");
+			console.notify("Waiting");
 			window.plugins.twitter.isTwitterAvailable(function(r) {
 				console.notify(r);
 			});
 		},
+        isSetup : function() {
+ 			console.notify("waitin...");
+ 			window.plugins.twitter.isTwitterSetup(function(r) {
+                console.notify(r);
+            });
+        },
+        
 	}
     document.addEventListener("deviceready", function(){
         if(twit.isAvailable()) {
-                           
+                console.notify("@test1");
         }
-                           
     }, false);
 
 })();
