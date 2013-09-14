a3de69ecbabcaacc2ee832f2e785f95cddb0a359
alfredkam <kam.alfred@gmail.com>
2013-09-14 08:34:50 -0700

    started to migrate tablet ux

--- assets/scripts/main/PHYLO-main.js
+++ assets/scripts/main/PHYLO-main.js
@@ -198,10 +198,11 @@ require(['yepnope'],
 	}
 );
 
-require(
-[ "marionette", "mustache", "scripts/App","jquery"],
-function(Marionette, Mustache, App)
-{
+require([ 
+	"marionette", "mustache", "scripts/App","jquery", "bootbox"
+], function(
+	Marionette, Mustache, App
+){
 	// var consoleUtils = new WebConsoleUtils({
 	// debug: config.DEBUG
 	// });
@@ -227,7 +228,6 @@ function(Marionette, Mustache, App)
 require(['dev']);
 require(['views/detectIE.actions']);
 // require(['views/customizeGame.actions']);
-require(['bootbox']);
 window.setTimeout(function() {
-require(['validation/login.validation']);
-},500);
+// require(['validation/login.validation']);
+// },500);
