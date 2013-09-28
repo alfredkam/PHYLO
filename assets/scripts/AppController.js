4afebf2a67d61cf56ce8a0e9a7969b6a554c19e2
alfredkam <akam@adcnx.com>
2013-09-28 10:31:06 -0400

    modified tablet login css

--- assets/scripts/AppController.js
+++ assets/scripts/AppController.js
@@ -78,7 +78,7 @@ define([
 				this.currentRoute = "/";
 				this.isInit = true;
 				this.user = (new Backbone.Model({
-					name : ""
+					name : "Alfred Kam"
 				}));
 
 				this.user.on("change", this.updateHeader, this);
@@ -141,19 +141,6 @@ define([
 				}));
 			}
 
-			// if (!this.regions.headerRegion.currentView)
-			// {
-			// 	this.regions.headerRegion.reset();
-			// 	this.regions.headerRegion.show(new HeaderView({
-			// 		model : this.langModel,
-			// 		lang : lang
-			// 	}));
-			// }
-
-			// if (!this.regions.footerRegion.currentView)
-			// {
-				
-			// }
 			this.currentLang = lang;
 			this.currentPage = pageName;
 			this.setActiveLink(pageName);
@@ -173,46 +160,6 @@ define([
 				$(this).attr("href", "#!/"+lang+"/"+pageName);
 			});
 		},
-		/* use this if to set up login */
-		/*
-		index : function()
-		{
-			var result = this.checkLogin();
-			if (result)
-			{
-				// go to default page
-				// for now let it be register page
-				this.initHeaderFooter();
-				// set route here if needed
-				// this.currentRoute = "#/performance";
-				// this.performance();
-
-				// oh also.. don't forget to initialize header and footer
-			}
-			else
-			{
-				// Not logged in
-				// show login page
-				// this.currentRoute = "#/login";
-				// this.login();
-			}
-		},
-		checkLogin : function()
-		{
-			// var isLoggedIn = false;
-			// this.user.fetch({
-			// async : false,
-			// success : function(model, response) {
-			// console.log("logged In");
-			// // Logged in, stay in current route unless you are at login
-			// // view
-			// isLoggedIn = true;
-			// }
-			// });
-			//
-			// return isLoggedIn;
-			return false;
-		}, */
 		login : function()
 		{
 			console.log("rendering login view");
