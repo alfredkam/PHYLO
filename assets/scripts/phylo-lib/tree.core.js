d916f499f583329fc7fc1373c7aef152568a8392
alfredkam <akam@adcnx.com>
2013-09-28 22:28:08 -0400

    updated tree.core

--- assets/scripts/phylo-lib/tree.core.js
+++ assets/scripts/phylo-lib/tree.core.js
@@ -157,7 +157,7 @@
 				}
 				
 				$("#ancestorSeq").html(seq);
-				$.customize.default();
+				// $.customize.default();
 				$("#ancestorSeq").show("slide",{direction : "left"},500);
 			},function() {
 				$("#ancestorSeq").hide();
