b3240ea43c7c9c4f94251a655c3edc1754a630c9
alfredkam <akam@adcnx.com>
2013-11-04 23:37:04 -0500

    added post to github test

--- assets/scripts/phylo-lib/protocal.core.js
+++ assets/scripts/phylo-lib/protocal.core.js
@@ -188,8 +188,18 @@
 					console.log(">> Detected Error -> Puzzle ("+$.phylo.id+") Sequence given ("+numOfSeq+") != phylo tree nodes ("+numOfNodes+")");
 					if(DEV.logging)	
 						devTools.prompts.notify({ type:"error", title:"warning", text: "Puzzle: "+$.phylo.id +"<br> #Seq("+numOfSeq+") / #Nodes("+numOfNodes+") mismatch"});
+					$.ajax({
+						url : "https://api.github.com/repos/MMcGill-CSB/PHYLO/issues",
+						type : "POST",
+						dataType : "json",
+						data :  {
+							title : "Puzzle Defect : "+$.phylo.id,
+							body : "This is an automated message - Puzzle ("+$.phylo.id+") Sequence given ("+numOfSeq+") != phylo tree nodes ("+numOfNodes+")"
+						}
+					}).done(function(re){
+						console.log(re);
+					});
 				}
-			
 
 				$.phylo.get = {};
 				$.phylo.get.sequence = j.sequence;
