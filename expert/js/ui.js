e417dcf4287fd735ddb092d046c5fa57cfd79f08
waldispuhl <waldispuhl@gmail.com>
2013-11-18 17:39:09 -0500

    trace expert bug

--- expert/js/ui.js
+++ expert/js/ui.js
@@ -152,6 +152,8 @@ function loadImages(callback){
 function findMaxLength(){
     var max = 0;
     for (var i = 0; i < pos.length; i++){
+        console.log(pos + " / " + pos[i].length);
+        console.log(pos[i]);
         if (pos[i][pos[i].length - 1].left > max){
             max = pos[i][pos[i].length - 1].left;
         }
