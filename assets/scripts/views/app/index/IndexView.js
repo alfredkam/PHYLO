5643a030a401cb6a656faa23c60f5be0d831da3a
arthurkam <arthur.kam@hotmail.com>
2014-01-18 13:30:40 -0500

    reindent, update redo and undo functions
    now with opacity changes depending on the states
    also the queue is implemented properly now (i hope)

--- assets/scripts/views/app/index/IndexView.js
+++ assets/scripts/views/app/index/IndexView.js
@@ -20,6 +20,7 @@ define([
          template : tpl,
          moveQueue : [],
          currMove : 0,
+         queueSize: 6,
          events:{
             "click #volume i":"toggleVolume",
             "newMove #moveListener":"newMove",
@@ -45,14 +46,15 @@ define([
             this.currMove++;
             if(this.moveQueue.length>this.currMove){
                 console.log("length reduced");
-                this.moveQueue = this.moveQueue.slice(this.currMove-5>1?this.currMove-5:0,this.currMove);
-                this.currMove  = this.moveQueue.length+1;
+                this.moveQueue = this.moveQueue.slice(this.moveQueue.length-this.queueSize>0?this.moveQueue.length-this.queueSize:0,this.currMove);
+                this.currMove  = this.moveQueue.length;
 
             }
             if(DEBUG){
                 console.log(this.currMove);
             }
-            //}            
+            //} 
+            this.undoRedoButtonsStatus();
          },
          newGame : function(){
             //quick reset for the move queue and position holder
@@ -71,32 +73,52 @@ define([
             // console.log(this.currMove-2, this.moveQueue[this.currMove-2]);
             
             $.helper.copy($.sequence.track, this.moveQueue[this.currMove-2]);
-                            var score = $.fitch.score();
-                            $.board.score(score);
-                            $.physics.snapRandom();
-                            if ($.phylo.bestScore >= $.sequence.par)
-                                $.board.approve();
-                            $.board.stats();
+            var score = $.fitch.score();
+            $.board.score(score);
+            $.physics.snapRandom();
+            if ($.phylo.bestScore >= $.sequence.par)
+                $.board.approve();
+            $.board.stats();
             this.currMove--;
-         },
+            this.undoRedoButtonsStatus();
+
+        },
          redoMove: function(){
             if(DEBUG){
 
                 console.log(this.currMove);
             }
             if(this.currMove+1>this.moveQueue.length){
-                            return;
-                        }
+                return;
+            }
             // console.log(this.currMove, this.moveQueue[this.currMove]);
-                       
-                       $.helper.copy($.sequence.track, this.moveQueue[this.currMove]);
-                                       var score = $.fitch.score();
-                                       $.board.score(score);
-                                       $.physics.snapRandom();
-                                       if ($.phylo.bestScore >= $.sequence.par)
-                                           $.board.approve();
-                                       $.board.stats();
-                                       this.currMove++;
+
+            $.helper.copy($.sequence.track, this.moveQueue[this.currMove]);
+            var score = $.fitch.score();
+            $.board.score(score);
+            $.physics.snapRandom();
+            if ($.phylo.bestScore >= $.sequence.par)
+               $.board.approve();
+           $.board.stats();
+           this.currMove++;
+           this.undoRedoButtonsStatus();
+         },
+         //a function to use so the buttons are in the right state
+         undoRedoButtonsStatus : function(){
+            //redo button
+            if(this.currMove+1>this.moveQueue.length){
+                $("#redo").removeClass("on");
+            }
+            else{
+                $("#redo").addClass("on");
+            }
+            if(this.currMove-2<0){
+                $("#undo").removeClass("on");
+            }
+            else{
+                $("#undo").addClass("on");
+
+            }
          },
          toggleVolume: function(e){
             var vol=0;
