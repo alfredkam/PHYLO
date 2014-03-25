eae4ea7bf26144da1fea861565ae8bf46b4e81a4
alfredkam <akam@adcnx.com>
2014-03-25 07:20:47 -0400

    ommit dist snapshot + set mode to dev

--- dist/expert/js/lib/jquery-ui-1.8.17.custom/development-bundle/ui/jquery.effects.fade.js
+++
@@ -1,1 +0,0 @@
-!function(a){a.effects.fade=function(b){return this.queue(function(){var c=a(this),d=a.effects.setMode(c,b.options.mode||"hide");c.animate({opacity:d},{queue:!1,duration:b.duration,easing:b.options.easing,complete:function(){b.callback&&b.callback.apply(this,arguments),c.dequeue()}})})}}(jQuery);
